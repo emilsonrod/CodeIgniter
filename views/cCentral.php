d11a8649c45389bff73b883445131a79399b1fef
Emilson Rodriguez <emilsonrod@gmail.com>
2014-04-07 19:58:28 +0200

    aca subo el proyecto para probarlo, con algunos avances

---
+++ views/cCentral.php
@@ -0,0 +1,5 @@
+<div id="columnacentral">
+                    
+    <h2>Bienvenido a Tis</h2>
+
+</div>
---
+++ views/home.php
@@ -0,0 +1,5 @@
+<?php $this->load->view('header');?>
+<?php $this->load->view('cIzquierda');?>
+<?php $this->load->view('cCentral');?>
+<?php $this->load->view('cDerecha');?>
+<?php $this->load->view('footer');?>
