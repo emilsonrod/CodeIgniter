b8c2437c262a2a88af9fe01c654071aca9106df0
Emilson Rodriguez <emilsonrod@gmail.com>
2014-04-10 09:59:05 +0200

    subo mi cambio

--- CodeIgniter/application/controllers/login.php
+++ CodeIgniter/application/controllers/login.php
@@ -5,43 +5,40 @@ class Login extends CI_Controller
 	{
 		parent::__construct();
 		$this->load->helper('form');
-		$this->load->model('modelLogin');
+		//$this->load->model('modelLogin');
 		$this->load->library('form_validation');
 	}
-	public function index($error = '')
-	{
-		$data['error'] = $error;
-		$this->load->view('login',$data);	
-		
-	}	
-	public function loggin()
+	public function index()
 	{
 		$this->form_validation->set_rules('nombre', 'nombre', 'trim|required|max_length[15]');
-        $this->form_validation->set_rules('passw', 'passw', 'trim|required|max_length[15]');
+	    $this->form_validation->set_rules('passw', 'passw', 'trim|required|max_length[15]');
 
-        $this->form_validation->set_message('required', 'El campo %s es obligatorio');
-        $this->form_validation->set_message('max_length', 'El Campo %s debe tener un Maximo de %d Caracteres');
+	    $this->form_validation->set_message('required', 'El campo %s es obligatorio');
+	    $this->form_validation->set_message('max_length', 'El Campo %s debe tener un Maximo de %d Caracteres');
 
-        if ($this->form_validation->run() == FALSE)
+	    if ($this->form_validation->run() == FALSE)
 		{
-			$this->load->view('login',$data);
+			$this->load->view('login');
+				//$this->index($error);
 		}
 		else
 		{
-			
 			$nombre = $this->input->post('nombre');
 			$passw = $this->input->post('passw');
-			
-			$insert = $this->modelLogin->logginUser($nombre, $passw);
-			if($insert)
-			{
-				redirect('/inicio/','refresh');
-			}
-			else
-			{
-				$this->load->view('login');
-			}			
-		}		
+				
+				//$insert = $this->modelLogin->logginUser($nombre, $passw);
+				/*if($insert)
+				{
+					$this->load->view('login');
+				}
+				else
+				{
+					//$this->load->view('login');
+					$this->index();
+				}*/	
+					
+		}
+		
 	}
 }
 ?>
