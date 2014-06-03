01a6eedc010ff38ad49615c59f921db2df656643
ellie44 <elizabeth.bravoflores@gmail.com>
2014-06-03 00:11:47 -0700

    subir documentos

--- application/models/modelSubirDoc.php
+++ application/models/modelSubirDoc.php
@@ -1,6 +1,6 @@
 <?php
 /**
-* 
+* SUBIR DOCUMENTOS
 */
 class ModelSubirDoc extends CI_Model
 {
@@ -9,19 +9,16 @@ class ModelSubirDoc extends CI_Model
 	{
 		parent::__construct();
 	}
-
-	//METODO QUE SIRVE PARA REGISTRAR DOCUMENTOS
-	function agregarDoc($id, $COD, $nombre, $descripcion, $tipo, $estado,$fecha)
+	//registrar documentos subidos 
+	function agregarDoc($CODG, $IDE, $nombre, $descripcion,$fecha)
 	{
-		$Datos = array('ID_USUARIO' =>$id,
-					   'COD_GRUPO' =>$COD,
-					   'NOMBRE_DOC' =>$nombre,
-					   'DESCRIPCION' => $descripcion,
-					   'TIPO' => $tipo,
-					   'ESTADO' => $estado,
-					   'FECHA' => $fecha,
+		$Datos = array('COD_GRUPO' => $CODG,
+					   'ID_EVENTO' => $IDE,
+					   'NOMBRE_ENTREGA' =>$nombre,
+					   'FECHA_ENTREGA' => $fecha,
+					   'COMENTARIO' => $descripcion,
 					   );
-		$this->db->insert('documentos', $Datos);
+		$this->db->insert('entrega', $Datos);
 
 		if ($this->db->affected_rows() == '1')
 		{
@@ -30,39 +27,15 @@ class ModelSubirDoc extends CI_Model
 			return FALSE;
 		}
 	}
-	//METODO QUE OBTINE TODOS LOS DOCUMENTOS SUBIDOS
-	function verLista()
+	//subir Doc Docentes
+	function agregarDocDoc($id_usuario, $nombre, $descripcion,$fecha)
 	{
-		$query = $this->db->get('documentos');
-		if($query ->num_rows() > 0)
-		{
-			return $query;
-		}
-		else{
-			return FALSE;
-		}
-	}
-	
-	//CONSULTA ARCHIVO POR ID
-	function consultarDoc($idArchivo)
-	{
-		$this->db->select('*');
-		$this->db->from('documentos');
-		$this->db->where('COD_DOCUMEN',$idArchivo);
-		$query = $this->db->get();
-		if($query ->num_rows() > 0)
-		{
-			return $query;
-		}
-		else{
-			return FALSE;
-		}
-	}
-	//BORRAR DOCUMNTOS POR ID ARCHIVO
-	function borrarDoc($idArchivo)
-	{
-		$this->db->where('COD_DOCUMEN',$idArchivo);
-		$this->db->delete('documentos');
+		$Datos = array('ID_USUARIO' => $id_usuario,
+					   'NOMBRE_DOC' =>$nombre,
+					   'FECHA_SUBIDA' => $fecha,
+					   'DESCRIPCION' => $descripcion,
+					   );
+		$this->db->insert('documento_docente', $Datos);
 
 		if ($this->db->affected_rows() == '1')
 		{
@@ -71,56 +44,68 @@ class ModelSubirDoc extends CI_Model
 			return FALSE;
 		}
 	}
-	//CONSULTAR TODO CON EL COD DE GRUPO
-	function consultarAll($cod_grupo)
-	{
-		$this->db->select('*');
-		$this->db->from('documentos');
-		return $this->db->get(); 
-	}
-	/*--------------------conseguir el grupo del q subio el doc--------------------*/
-	function getIdGrupo($id)
-	{
-		$sql="select COD_GRUPO from integrantes_grupo where ID_USUARIO='".$id."'";
-		$consulta = $this->db->query($ql);
-		$fila = $consulta->row();
-		return $fila->cod_grupo;
-	}
-
-	/*-----------------*/
-	function getCodGrupos($id)
+	//obtener el grupo del q inicio sesion 
+	function getCodGrupo($id)
 	{
 		$sql="select * from integrantes_grupo where id_usuario=".$id;
 		$grupos=$this->db->query($sql);	
 		
-		return $grupos;		
+		return $grupos;
 	}
+	//verifica si esta inscrito o no
+	function inscrito($id){
+        $query= $this->db->query("SELECT * FROM integrantes_grupo WHERE id_usuario=".$id);
+        if ($query->num_rows() >0)
+        {
+            return true;
+        }else{
+            return false;
+        }
+    }
+    //verificar si hay evento para el grupo y si el evento es de TIPO SOLO DOCUMENTACION y si aun estamos en fecha para entregar
+    function verificarEventoDoc($id_doc)
+    {
+    	$sql = "select * from evento where fecha_evento >= current_date  and id_usuario = '".$id_doc."' order by fecha_evento limit 1 "; 
+    	$consulta = $this->db->query($sql);
 
-	function getDocGrupo($id)
-	{
-		$sql="select * from documentos where id_usuario='".$id."' order by cod_documen desc limit 1";
-		$grupos=$this->db->query($sql);	
-		
-		return $grupos;		
-	}
-	function insertaDocGrupo($id_fecha, $id_doc, $id_grupo)
-	{
-		$Datos = array( 'ID_FECHA' => $id_fecha,
-						'COD_DOCUMEN' => $id_doc,
-						'COD_GRUPO' =>$id_grupo
-					   );
-		$this->db->insert('documentos_grupo', $Datos);
-
-		if ($this->db->affected_rows() == '1')
+		if($consulta ->num_rows() > 0)
 		{
-			return TRUE;
-		}else{
+			return $consulta;
+		}
+		else{
 			return FALSE;
 		}
-	}
-	function getDocumentosGrupo($grupo){
-		$sql="select * from documentos 
-			where cod_grupo='".$grupo."'";
+    }
+    function obtenerDocente($grupo)
+    {
+    	$sql = "select * from grupo where cod_grupo = ".$grupo ;
+    	$consulta = $this->db->query($sql);
+
+		if($consulta ->num_rows() > 0)
+		{
+			return $consulta;
+		}
+		else{
+			return FALSE;
+		}
+
+    }
+	//listar fechas eventos para eventos.
+	 function listaEventos($id_doc, $fecha)
+	 {
+    	$sql="SELECT te.nombre_tipo_evento  FROM evento e, tipo_evento te where e.id_usuario = '".$id_doc."' and e.fecha_evento >= current_date  and e.fecha_evento = '".$fecha."' and e.id_tipo_evento = te.id_tipo_evento  order by fecha_evento";
+		$query = $this->db->query($sql);        
+        $lista=array();
+		foreach ($query->result_array() as $row)
+				{
+					$lista[$row['nombre_tipo_evento']]=$row['nombre_tipo_evento'];
+				}
+		return $lista;
+    }
+    //obtenr id del evnto para ingresar en entrega
+    function getEvento($nombre_tipo_evento, $id_doc)
+    {
+    	$sql = "select e.id_evento from evento e, tipo_evento te where te.nombre_tipo_evento = '".$nombre_tipo_evento."' and te.id_tipo_evento = e.id_tipo_evento and e.id_usuario = '".$id_doc."'";
 		$consulta = $this->db->query($sql);
 
 		if($consulta ->num_rows() > 0)
@@ -130,85 +115,6 @@ class ModelSubirDoc extends CI_Model
 		else{
 			return FALSE;
 		}	
-
-		//return $this->db->query($sql);
-	}
-	function getDocumentosGrupoFecha($grupo)
-	{
-		$sql="select DISTINCT d.cod_documen, d.nombre_doc, d.descripcion, d.tipo, d.estado, d.fecha, fl.comentario from documentos d, documentos_grupo dg, fecha_limite fl
-			where dg.cod_grupo='".$grupo."' and  d.cod_grupo = dg.cod_grupo and dg.id_fecha = fl.id_fecha";
-
-		$consulta = $this->db->query($sql);
-
-		if($consulta ->num_rows() > 0)
-		{
-			return $consulta;
-		}
-		else{
-			return FALSE;
-		}	
-		
-	}
-	function getDocumentosDoc($id_usuario){
-		$sql="select * from documentos 
-			where id_usuario='".$id_usuario."'";
-		$consulta = $this->db->query($sql);
-
-		if($consulta ->num_rows() > 0)
-		{
-			return $consulta;
-		}
-		else{
-			return FALSE;
-		}	
-		//return $this->db->query($sql);
-		
-	}
-	function rolUsuario($id_usuario)
-	{
-		$sql = "select * from rol_usuario where id_rol='1' and id_usuario='".$id_usuario."'";
-
-		$consulta = $this->db->query($sql);
-
-		if($consulta ->num_rows() > 0)
-		{
-			return TRUE;
-		}
-		else{
-			return FALSE;
-		}
-	}
-	//VERIFICA SI HAY UNA FECHA LIMITE PARA LA ENTREGA DEL DOCUEMENTO
-	function  verificarFecha()
-	{
-		$sql = "select * from fecha_limite where fecha >= current_date order by fecha limit 1";
-		$consulta = $this->db->query($sql);
-
-		if($consulta ->num_rows() > 0)
-		{
-			return $consulta;
-		}
-		else{
-			return FALSE;
-		}
-		
-	}
-	function verificarArchivoSubido($fecha)
-	{
-		$sql = "select * from fecha_limite where fecha <= '".$fecha."' order by fecha desc limit 1";
-		$consulta = $this->db->query($sql);
-		if($consulta ->num_rows() > 0)
-		{
-			return $consulta;
-		}
-		else{
-			return FALSE;
-		}
-	}
-	//actualizar la base de datos de los documentos para verificar que despues de la fecha de entrega se desabilite la opcion eliminar
-
-	function actualizarEstadoDoc($fecha)
-	{
-		$sql ="UPDATE documentos SET ESTADO = 0 WHERE FECHA = current_date ";
-	}
+    }
 }
+?>
--- application/views/viewSubirDoc.php
+++ application/views/viewSubirDoc.php
@@ -1,111 +1,43 @@
 <?php $this->load->view('viewCabecera');?>
-<?php $this->load->view('viewIzquierda');?>
-<!--SUBIR DOCUMENTO-->
+<?php $this->load->view('viewIzquierda',$tareas);?>
 <div id="columnacentral">
 	
-			<!-- CSS -->
-		<link rel="stylesheet" href="estilos/estilos.css" type="text/css" />
-		<link href="http://fonts.googleapis.com/css?family=Aclonica:regular" rel="stylesheet" type="text/css" >
+	<h1>Subir documento</h1>
+	<div id="contenedorSubirDoc">	
 
-		<!-- Libreria JQuery -->
-		<script src="js/jquery-1.7.2.min.js" type="text/javascript"></script><!--se modifica por la version de jquery que tengan -->
-        
-        <!--PARA SUBIR ARCHIVOS -->
-        <link rel="stylesheet" href="uploadify/uploadify.css" type="text/css" />
-		<script type="text/javascript" src="uploadify/jquery.uploadify.js"></script>
-		
-<script type="text/javascript">
-	$(document).ready(function() 
-	{	
-	    $("#file_upload").fileUpload({
-	        'uploader': 'uploadify/uploader.swf',
-	        'cancelImg': 'uploadify/cancel.png',
-	        'script': 'subirDoc/agregarDoc',
-	
-	        'folder': 'uploads',
-	        'buttonText': 'Examinar',
-	        'checkScript': 'uploadify/check.php',
-	        'fileDesc': 'Archivos',
-	        'sizeLimit': 20971520 ,
-	        'auto':false,
-	        //Estensiones permitidas para carga de los archivos
-	        //'fileExt': '*.jpg;*.jpeg;*.gif;*.png;*.xlsx;*.docx;*.html;*.txt;*.pdf;*.zip',
-	        'fileExt': '*.docx;*.txt;*.pdf;*.zip',
-			
-			//Puede ingresar mas de 1 archivo a la vez, false solo 1 archivo
-	        'multi': false,
-	        'displayData': 'percentage',
-	        /*onComplete: function (){
-	     verlistadoimagenes();
-	            $("#txtdes").val('');
-	        }*/
-	
-	       });
-	   $('#txtdes').bind('change', function(){
-		$('#file_upload').fileUploadSettings('scriptData','&des='+$(this).val());
-	
-	
-	    });
-	
-	})
-	
-	function startUpload(id, conditional){
-		if(conditional.value.length != 0) {
-			$('#'+id).fileUploadStart();
-		} else
-			alert("Debe ingresar una descripcion");
-	}
-
-</script>
-	
-	<!--div id="tituloSubirDoc"><h1>SUBIR DOCUMENTOS :</h1></div-->
-
-	<?php
-	if(!$docente)
-	{
-		if($fechas)
-		{
-			foreach ($fechas->result() as $fila) 
-			{
-			    echo "<div id='tituloSubirDoc'>";
-				    echo "<h3>Documentos para la fase: ".$fila->COMENTARIO."</h3>";
-					echo "<h3>Este documentos se entregara hasta la fecha : ".$fila->FECHA."</h3>";
-					echo "<span>Pasada la fecha no se podra entregar el documento o iniciara la entrega del siguiente hito</span>";
-				echo "</div>";
-			}
-		}
-	}
-		
- 	?>
-	<div id="contenedorSubirDoc">		
 		<div id="formsubirDoc"></div>
-		     <form>
+		
+			<?php echo form_open_multipart('subirDocEst');?>
 				<fieldset>
-					<legend>Descripcion del documento:</legend>
-					<textarea class="cajas" cols="" name="txtdes" id="txtdes" style="width: 400px;height: 80px" ></textarea>
-					<!--?php echo form_textarea(array('class' =>'subirDocDesc' ,'name' => 'txtDesc' , 'id' => 'txtDesc', 'style' =>'width:400px;height:80px'))?-->
+					<legend>Elegir evento :</legend>
+						<p>
+						<?php echo form_label('Eventos recientes:', 'fecha');?>
+   				 		<?php echo form_dropdown('fecha', $lista, set_value('fecha')); ?>
+					<p/>
+					<?php echo $error;?>
 				</fieldset>
+
 				<fieldset>
-					<legend>Elegir Archivo :</legend>
-					<input id="file_upload" type="file" name="file_upload" />
-					<!--?php echo form_input(array('id' => 'archivoUsuario', 'type' => 'file', 'name' => 'archivoUsuario', 'size'=>'20' ))?-->
+				<legend>Elegir documento:</legend>
+					<span>El formato de los archivos permitidos son .pdf,.doc </span><br/>
+					<span>El tamaño permitido es de 20 mb.</span>
+					<p>
+						<input type="file" name="userfile" id="userfile"/>
+					<p/>
+			</fieldset>
+				<fieldset>
+				<legend>Descripcion del documento:</legend>
+				<span>Numero de caracteres permitidos 150.</span><br/>
+				<?php echo form_textarea(array('class' =>'cajas' ,'name' => 'txtdes' ,'maxLength' => '150', 'id' => 'txtdes', 'style' =>'width:400px; height:80px'))?>
+				<br/>
+				<h5><?php echo form_error('txtdes');?></h5>
 				</fieldset>
-				<div aling="right">
-					<input class="button" type="button" value="Cargar" onclick="javascript:startUpload('file_upload', document.getElementById('txtdes'))"/>
-					<!--?php echo form_submit('regDoc', 'Subir Documento')?-->
-				</div>
+			<div aling="right">
+				<input class = "button" type="submit" name="submit" value="Subir Documento" />
+			</div>
 			</form>
-		</div>
-		<div id = "barra1">
-	        <ul>
-	        	<li><a href="listarDoc">Listar Documentos</a></li>
-	        </ul>
-       </div> 
+			
+		</div> 
 	</div>
-		
-
-		
-<!--FIN-->
-
 <?php $this->load->view('viewDerecha');?>
 <?php $this->load->view('viewPiePagina');?>
