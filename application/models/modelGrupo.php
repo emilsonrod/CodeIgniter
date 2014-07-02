1a640e4926c899c4dddf1683c919046cd00fc406
franklin <frank77inf@gmail.com>
2014-07-01 22:06:00 -0600

    pequeñas correcciones q me olvide realizar listo ....

--- application/models/modelGrupo.php
+++ application/models/modelGrupo.php
@@ -25,6 +25,7 @@ class ModelGrupo extends CI_Model {
             $form['nombre_largo']=$form_data['nombreLargo'];
 			$form['nombre_corto']=$form_data['nombreCorto'];
 			$form['passw_grupo']=$form_data['contrasenya'];
+            $form['activo']='1';
 			$this->db->insert('grupo',$form);
 
 		if ($this->db->affected_rows() == '1')
